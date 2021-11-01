f80aa7cde41e04805da59f8b37b136e87ddea612
AMGavrilov <amgavri5@mts.ru>
2021-11-01 16:53:27 +0300

    hide email sub

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,9 +93,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
         </div>
       )}
       {siteMetadata.newsletter.provider !== '' && (
-        <div className="flex items-center justify-center pt-4">
-          <NewsletterForm />
-        </div>
+        <div className="flex items-center justify-center pt-4">{/*<NewsletterForm />*/}</div>
       )}
     </>
   )
