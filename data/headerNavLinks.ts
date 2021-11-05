1ee71e442686150c7d0542f400b51521726ea7d7
AMGavrilov <amgavri5@mts.ru>
2021-11-05 20:48:40 +0300

    feat: add main page self present component

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Статьи' },
   { href: '/tags', title: 'Теги' },
   { href: '/projects', title: 'Проекты' },
-  { href: '/about', title: 'Обо мне' },
+  { href: '/about', title: 'Об авторе' },
 ]
 
 export default headerNavLinks
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,88 @@
+import React from 'react'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
+
 import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
-import { getAllFilesFrontMatter } from '@/lib/mdx'
+import { getAllFilesFrontMatter, getFileBySlug } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
-import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import { PostFrontMatter } from 'types/PostFrontMatter'
-import NewsletterForm from '@/components/NewsletterForm'
+import Image from '@/components/Image'
+import SocialIcon from '@/components/social-icons'
+import { AuthorFrontMatter } from 'types/AuthorFrontMatter'
 
 const MAX_DISPLAY = 5
 
-export const getStaticProps: GetStaticProps<{ posts: PostFrontMatter[] }> = async () => {
+// @ts-ignore
+export const getStaticProps: GetStaticProps<{
+  posts: PostFrontMatter[]
+  authorDetails: AuthorFrontMatter
+}> = async () => {
   const posts = await getAllFilesFrontMatter('blog')
+  const authorDetails = await getFileBySlug<AuthorFrontMatter>('authors', ['default'])
+  const { frontMatter } = authorDetails
 
-  return { props: { posts } }
+  return {
+    props: {
+      posts,
+      authorDetails: frontMatter,
+    },
+  }
 }
 
-export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home({
+  posts,
+  authorDetails,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const {
+    avatar,
+    company,
+    email,
+    github,
+    instagram,
+    linkedin,
+    name,
+    occupation,
+    youtube,
+  } = authorDetails
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Самое интересное
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p>
+          {/*<h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">*/}
+          {/*  Самое интересное*/}
+          {/*</h1>*/}
+          {/*<p className="text-lg leading-7 text-gray-500 dark:text-gray-400">*/}
+          {/*  {siteMetadata.description}*/}
+          {/*</p>*/}
+
+          <div className="items-center flex flex-col">
+            <div className="flex">
+              <Link href="/about">
+                <Image
+                  src={avatar}
+                  alt="avatar"
+                  width="150px"
+                  height="150px"
+                  className="w-48 h-48 rounded-full"
+                />
+              </Link>
+            </div>
+            <div className="items-center flex flex-col">
+              <h3 className="pt-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
+              <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
+              <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            </div>
+            <div className="flex pt-4 space-x-4">
+              <SocialIcon kind="mail" href={`mailto:${email}`} />
+              <SocialIcon kind="github" href={github} />
+              <SocialIcon kind="linkedin" href={linkedin} />
+              <SocialIcon kind="instagram" href={instagram} />
+              <SocialIcon kind="youtube" href={youtube} />
+            </div>
+          </div>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
